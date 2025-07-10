f68f4e9b9c08c556f5e31fd31594d4ab7ac3dba6
Akeem Bashiru <akeembashiru@Akeems-Mac-mini.local>
2025-07-10 14:56:44 +0300

    Updates

--- _next/static/LTBvNGhAPAMbekFCNXT1V/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-b2ba1b4d03c5552d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
