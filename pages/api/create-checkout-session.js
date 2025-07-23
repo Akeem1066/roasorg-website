253251e6e214d41e32f102f126dac09fc53f4b0e
Akeem Bashiru <akeembashiru@Akeems-Mac-mini.local>
2025-07-24 00:34:51 +0300

    Fix success_url and cancel_url for checkout session

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -32,8 +32,8 @@ export default async function handler(req, res) {
         },
       ],
       mode: 'payment',
-      success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${req.headers.origin}/cancel`,
+      success_url: `${req.headers.origin || 'https://roasorg.com'}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.origin || 'https://roasorg.com'}/cancel`,
       metadata: {
         description: description,
       },
